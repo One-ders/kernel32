64867b7ad9d1ec0ec1daad6b31ecf612f227b01f
Anders Franzen <anders.franzen@yahoo.se>
2019-12-29 23:47:10 +0100

    changed CEC pin, for use of broken board

--- bl2/boards/MB997C/config.h
+++ bl2/boards/MB997C/config.h
@@ -34,10 +34,11 @@
 #define LED_BLUE	8
 
 /* Cec */
-#define CEC_PIN         GPIO_PIN(PC,4);
+//#define CEC_PIN         GPIO_PIN(PC,4);
+#define CEC_PIN         GPIO_PIN(PC,5);
 
 //#define WITH_SONY_A1
 /* Sony A1 */
 // previous use of PB0 changed, because of burned gpio pin
-#define A1_PIN          GPIO_PIN(PC,5);
+//#define A1_PIN          GPIO_PIN(PC,5);
 
